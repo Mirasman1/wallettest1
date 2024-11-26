994afc5588eadd66029478374c5bb0ba4030b05f
Mirasman1 <105727649+Mirasman1@users.noreply.github.com>
2024-11-27 01:04:58 +0300

    q

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/', // Match the root URL
+        destination: '/main.html', // Serve the static HTML file
+      },
+    ];
+  },
 };
 
 export default nextConfig;
